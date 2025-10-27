2a696f4631e7f9adb791f2a2f58b71a4e8aa34a5
Ryan Alfi <real.rezz@gmail.com>
2025-10-27 13:32:10 +0700

    Initial Commit

---
+++ vertical/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vertical
+//
+//  Created by Ari Fajrianda Alfi on 27/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
